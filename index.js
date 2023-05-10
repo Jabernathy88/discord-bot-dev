a91804b08c50b154f8dd3a433da997fc0869aeee
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-05-10 19:01:21 -0400

    Set up development secret, dotenv module.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import { config } from "dotenv";
 import { Client } from "discord.js";
 
+config();
+
 const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
-const TOKEN = 'spacefiller'
+const TOKEN = process.env.JER_BOT_TOKEN;
 
 client.login(TOKEN)
