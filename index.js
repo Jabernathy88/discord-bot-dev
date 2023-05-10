37233415044010513c3a6750c0a3edffee18c3b6
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-05-10 19:21:55 -0400

    Successful test of Discord bot listen and log realtime user activity.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import { config } from "dotenv";
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 
 config();
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
-const TOKEN = process.env.JER_BOT_TOKEN;
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
 
-client.login(TOKEN)
+const TOKEN = process.env.JER_BOT_TOKEN; // bot authentication for discord api
+
+client.login(TOKEN);
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in!`) 
+});
+
+client.on('messageCreate', (message) => {
+    console.log(message.content);
+    console.log(message.createdAt.toDateString());
+    console.log(message.author.tag);
+});
+
+client.on('channelPinsUpdate', (channel, date) => {
+    
+})
